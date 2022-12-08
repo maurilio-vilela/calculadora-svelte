6dd510d6ea78753b5a9005b7e0461234d49ac693
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-08 13:19:46 -0300

    Adicionado configurações para deploy no GitHub Pages

---
+++ docs/_app/immutable/chunks/2-3a604fb5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-7bf0d695.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 import preprocess from 'svelte-preprocess';
+
+const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,10 +12,14 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		prerender: { entries: [] }
-	}
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/calculadora-svelte",
+        },
+		}
 };
 
 export default config;
