3785e892f0162c130ba4b597164ba6adcdfe681b
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-08 12:08:18 -0300

    Alterado a configuração em svelte.config para adapter-static para colocar em produção no GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,15 +8,9 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter({fallback: '200.html'
+    }),
+    prerender: { entries: [] }
 	}
 };
 
